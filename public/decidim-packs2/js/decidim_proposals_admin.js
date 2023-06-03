59b93bdcaa9d8407f8171ce535808e78a8b01668
nicoriva <nicoriva20@gmail.com>
2023-06-03 11:23:34 -0400

    decidim incial

---
+++ public/decidim-packs2/js/decidim_proposals_admin.js
@@ -0,0 +1,25 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "../../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-proposals-0.27.3/app/packs/entrypoints/decidim_proposals_admin.js":
+/*!**************************************************************************************************************************************!*\
+  !*** ../../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-proposals-0.27.3/app/packs/entrypoints/decidim_proposals_admin.js ***!
+  \**************************************************************************************************************************************/
+/***/ (function() {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: Cannot find package '@babel/plugin-proposal-private-methods' imported from /home/nico/pasantia/pasantia1/babel-virtual-resolve-base.js\n    at new NodeError (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:203:5)\n    at packageResolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:873:9)\n    at moduleResolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:902:20)\n    at defaultResolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:985:15)\n    at resolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:999:12)\n    at resolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/config/files/import-meta-resolve.js:13:10)\n    at tryImportMetaResolve (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/config/files/plugins.js:123:45)\n    at resolveStandardizedNameForImport (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/config/files/plugins.js:145:19)\n    at resolveStandardizedName (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/config/files/plugins.js:154:12)\n    at loadPlugin (/home/nico/pasantia/pasantia1/node_modules/@babel/core/lib/config/files/plugins.js:47:20)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["../../.rbenv/versions/3.0.2/lib/ruby/gems/3.0.0/gems/decidim-proposals-0.27.3/app/packs/entrypoints/decidim_proposals_admin.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=decidim_proposals_admin.js.map
